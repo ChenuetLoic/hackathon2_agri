7f1d4e0505edc7469a8be0d0473c9b0977e19578
Kronigeur <loicchenuet@hotmail.fr>
2021-01-13 15:40:18 +0100

    map finish

--- src/Controller/MapController.php
+++ src/Controller/MapController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\CityRepository;
+use App\Repository\FarmerRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,10 +15,15 @@ class MapController extends AbstractController
 {
     /**
      * @Route ("/", name="index_map", methods={"GET"})
+     * @param CityRepository $cityRepository
+     * @param FarmerRepository $farmerRepository
      * @return Response
      */
-    public function indexMap(): Response
+    public function indexMap(CityRepository $cityRepository, FarmerRepository $farmerRepository): Response
     {
-        return $this->render('map/map.html.twig');
+        return $this->render('map/map.html.twig', [
+        ]);
+
+
     }
 }
