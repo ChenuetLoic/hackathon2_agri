cdb33a501252c95af1196805bf82a62b5227856f
JulieRebeyrolle <julie.rebeyrolle@gmail.com>
2021-01-13 13:51:15 +0100

    add cities fixtures

---
+++ src/DataFixtures/CityFixtures.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use App\Entity\City;
+
+class CityFixtures extends Fixture implements ContainerAwareInterface
+{
+    private $container;
+
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $serializer = $this->container->get('serializer');
+        $filepath = realpath ("./") . "/src/DataFixtures/cities.csv";
+
+        $csvData = $serializer->decode(file_get_contents($filepath), 'csv');
+        $i = 0;
+
+        foreach ($csvData as $csvLine) {
+            $city = new City();
+            $city->setZipcode($csvLine['zipcode'])
+                ->setCity($csvLine['city'])
+                ->setLatitude($csvLine['lat'])
+                ->setLongitude($csvLine['long'])
+                ->setInseeCode($csvLine['insee_code']);
+            $this->addReference('city_' . $i, $city);
+            $manager->persist($city);
+            $i++;
+        }
+
+        $manager->flush();
+    }
+}
