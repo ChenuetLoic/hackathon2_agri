4635f9a45e031721e0cc4d07d2a1e05d4070bc53
Matthieu <bardetmatthieu@posteo.net>
2021-01-14 12:52:11 +0100

    fixed geolocation

--- webpack.config.js
+++ webpack.config.js
@@ -33,9 +33,6 @@ Encore
      * and one CSS file (e.g. app.scss) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/js/app.js')
-    .addEntry('geolocation', './assets/js/geolocation.js')
-    .addEntry('search', './assets/js/search.js')
-
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
